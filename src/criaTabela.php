a5efe4adeae90337ecb35bac00d42a5f2e9766e2
Gustavo_Nunes <gustavo.r.sousa@icloud.com>
2026-05-05 21:34:42 -0300

    Update criaTabela.php

--- src/criaTabela.php
+++ src/criaTabela.php
@@ -4,14 +4,8 @@ $sql = "CREATE TABLE IF NOT EXISTS produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
-    quantidade INT DEFAULT 0)";
-    create table login (
-    ID int auto_increment not null ,
-    EMAIL varchar (64) not null,
-    SENHA varchar (100) not null,
-    primary key (ID)
+    quantidade INT DEFAULT 0
 )";
-
 $pdo->exec($sql);
 echo "<h1>Tabela criada com sucesso!</h1>";
 //header("Location: relatorio.php?msg=tabela_pronta");
