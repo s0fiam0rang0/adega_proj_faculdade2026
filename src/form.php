0f58131f62f639543bc050ee605bc084c8d4fcfc
Gustavo_Nunes <gustavo.r.sousa@icloud.com>
2026-05-12 21:19:50 -0300

    Update form.php

--- src/form.php
+++ src/form.php
@@ -8,9 +8,9 @@
 <body>
     <img src="img_adega.jpeg">
     <h1>Adega UniNine</h1>
-    <form method="post" action="relatorio.php">
-    <input type="text" id="login" name="login" placeholder="Nome" required><br><br>
-    <input type="password" id="senha" name="senha" placeholder="Senha" required><br><br>
+    <form method="post" action="login.php">
+    <input type="text" id="login" name="EMAIL" placeholder="email" required><br><br>
+    <input type="password" id="senha" name="SENHA" placeholder="Senha" required><br><br>
     <div class="entrar"><input type="submit" value="Entrar"></div>
 
  </form>
